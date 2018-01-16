dd90d6e0354f68ce6237776aa129230e2d4abe00
Drake Lambert <drake.askme2@gmail.com>
2018-01-16 11:50:18 -0600

    initial template

---
+++ Junior/CSC4263/project1/Test Project 1/Assets/PaddleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviour : MonoBehaviour
+{
+    Vector3 initial;
+    Rigidbody2D pad;
+    BoxCollider2D coll;
+
+    // Use this for initialization
+    void Start()
+    {
+        pad = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        initial = pad.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pad.transform.Translate(new Vector3(0.3f, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pad.transform.Translate(new Vector3(-0.3f, 0, 0));
+        }
+        var collisions = new Collider2D[10];
+        coll.OverlapCollider(new ContactFilter2D(), collisions);
+
+    }
+}
