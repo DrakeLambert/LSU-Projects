41b6357d6a498c888b93f5ab976a1dd32322b0be
Drake Lambert <drake.askme2@gmail.com>
2018-01-28 23:20:51 -0600

    added 3d game

---
+++ Junior/CSC4263/Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float initVelocity = 1f;
+
+	private Rigidbody body;
+
+	private bool inPlay;
+
+	void Awake() {
+		body = GetComponent<Rigidbody> ();
+	}
+
+	void Update() {
+		if (Input.GetButtonDown ("Fire1") && !inPlay) {
+			transform.parent = null;
+			inPlay = true;
+			body.isKinematic = false;
+			body.AddForce (new Vector3 (initVelocity, initVelocity, 0));
+		}
+	}
+}
