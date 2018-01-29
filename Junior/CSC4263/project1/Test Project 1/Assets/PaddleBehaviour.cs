a3af99efce047bf7f2091d0b1951118cff60e4aa
Drake Lambert <drake.askme2@gmail.com>
2018-01-28 20:00:30 -0600

    added paddles

--- Junior/CSC4263/project1/Test Project 1/Assets/PaddleBehaviour.cs
+++ Junior/CSC4263/project1/Test Project 1/Assets/PaddleBehaviour.cs
@@ -4,31 +4,21 @@ using UnityEngine;
 
 public class PaddleBehaviour : MonoBehaviour
 {
-    Vector3 initial;
     Rigidbody2D pad;
-    BoxCollider2D coll;
+
 
     // Use this for initialization
     void Start()
     {
         pad = GetComponent<Rigidbody2D>();
-        coll = GetComponent<BoxCollider2D>();
-        initial = pad.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.Space))
         {
-            pad.transform.Translate(new Vector3(0.3f, 0, 0));
+            pad.AddTorque(300f);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            pad.transform.Translate(new Vector3(-0.3f, 0, 0));
-        }
-        var collisions = new Collider2D[10];
-        coll.OverlapCollider(new ContactFilter2D(), collisions);
-
     }
 }
---
+++ Junior/CSC4263/project1/Test Project 1/Assets/PaddleBehaviourL.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviourL : MonoBehaviour
+{
+    Rigidbody2D pad;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pad = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pad.AddTorque(-300f);
+        }
+    }
+}
