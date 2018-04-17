a318e2473516b33c3a30ac4d6d928bdcb9170495
Drake Lambert <drake.askme2@gmail.com>
2018-04-17 00:50:23 -0500

    hw4

--- Junior/CSC2610/Java P2/MagicSquareChecker.java
+++ Junior/CSC2610/Java P2/MagicSquareChecker.java
@@ -1,17 +1,61 @@
 import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class FileCopy {
+public class MagicSquareChecker {
     public static void main(String[] args) throws IOException {
-        byte[] buffer = new byte[1000];
+        Scanner fileIn = new Scanner(new File(args[0]));
+        System.out.println();
+        ArrayList<Integer> magicSquare = new ArrayList<>();
 
-        FileInputStream inputStream = new FileInputStream(args[1]);
-        FileOutputStream outputStream = new FileOutputStream(args[1] + " - 2");
-
-        int read = inputStream.read(buffer);
-        while (read != -1) {
-            outputStream.write(buffer, 0, read);
+        while (fileIn.hasNextInt()) {
+            magicSquare.add(fileIn.nextInt());
         }
-        inputStream.close();
-        outputStream.close();
+        int dimension = (int) Math.sqrt(magicSquare.size());
+        boolean containsAll = true;
+        for (int i = 1; i <= magicSquare.size(); i++) {
+            if (!magicSquare.contains(i)) {
+                containsAll = false;
+            }
+        }
+        if (dimension != Math.sqrt(magicSquare.size()) || !containsAll) {
+            System.out.println("Not a magic square");
+        } else {
+            int[] rowSums = new int[dimension], colSums = new int[dimension];
+            int sumDiagMinor = 0, sumDiagMajor = 0, row = dimension - 1, col = dimension / 2, currentCol = 1;
+            for (int index = 0; index < Math.pow(dimension, 2); index++) {
+                rowSums[index / dimension] = rowSums[index / dimension] + magicSquare.get(index);
+                colSums[(index % dimension)] = colSums[(index % dimension)] + magicSquare.get(index);
+                if (index / dimension == (index % dimension)) {
+                    sumDiagMajor = sumDiagMajor + magicSquare.get(index);
+                }
+                if (index / dimension + (index % dimension) == dimension - 1) {
+                    sumDiagMinor = sumDiagMinor + magicSquare.get(index);
+                }
+                System.out.printf("%-6d", magicSquare.get(index));
+                if (currentCol == dimension) {
+                    System.out.println();
+                    currentCol = 0;
+                }
+                currentCol++;
+            }
+            System.out.println();
+            if (sumDiagMajor == sumDiagMinor) {
+                boolean isMagicSquare = true;
+                for (int i = 0; i < rowSums.length; i++) {
+                    if (rowSums[i] != sumDiagMajor || colSums[i] != sumDiagMajor) {
+                        isMagicSquare = false;
+                        i = rowSums.length;
+                    }
+                }
+                if (isMagicSquare)
+                    System.out.printf("A %d by %d Magic Square%n", dimension, dimension);
+                else
+                    System.out.println("Not a magic square");
+            } else {
+                System.out.println("Not a magic square");
+            }
+            fileIn.close();
+        }
     }
 }
