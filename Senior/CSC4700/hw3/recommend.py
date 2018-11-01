2bb2da7808e8d0a39c823553fe793bd440bbdf56
Drake Lambert <drake.askme2@gmail.com>
2018-11-01 11:01:05 -0500

    formatted

--- Senior/CSC4700/hw3/recommend.py
+++ Senior/CSC4700/hw3/recommend.py
@@ -1,14 +1,16 @@
 from collections import defaultdict
 import math
 
-### define a function
+# define a function
+
+
 def mean(list_numbers):
     return float(sum(list_numbers)) / max(len(list_numbers), 1)
 
- 
-### code entry point: starting from reading files
 
-fi_ratings = open('rating.txt',"r").read()
+# code entry point: starting from reading files
+
+fi_ratings = open('rating.txt', "r").read()
 line_records = fi_ratings.split("\n")
 
 allRatings = []
@@ -16,28 +18,26 @@ userRatings = defaultdict(list)
 userLikedItems = defaultdict(list)
 
 for line in line_records[0:-1]:
-  spl = line.split(" ")
-  userID = spl[0]
-  itemID = spl[1]
-  rating = float(spl[2])
-  allRatings.append(rating)
-  userRatings[userID].append(rating)
-  if rating > 3.0:
-    userLikedItems[userID].append(itemID)
+    spl = line.split(" ")
+    userID = spl[0]
+    itemID = spl[1]
+    rating = float(spl[2])
+    allRatings.append(rating)
+    userRatings[userID].append(rating)
+    if rating > 3.0:
+        userLikedItems[userID].append(itemID)
 
-base_user_id='U845167387'
-print 'base user ratings, it is a list of ratings'
-print userRatings[base_user_id]
-print 'base user liked items, it is a list of game id'
-print userLikedItems[base_user_id]
+base_user_id = 'U845167387'
+print('base user ratings, it is a list of ratings')
+print(userRatings[base_user_id])
+print('base user liked items, it is a list of game id')
+print(userLikedItems[base_user_id])
 
-#Global average rating
-print 'the average rating of all ratings: {:.4f}'.format(mean(allRatings)) 
+# Global average rating
+print('the average rating of all ratings: {:.4f}'.format(mean(allRatings)))
 
-#Compute average rating of base user id
+# Compute average rating of base user id
 
-#Compute base user id neighbors whose jaccard similarity to base is larger than 0.2
+# Compute base user id neighbors whose jaccard similarity to base is larger than 0.2
 
-#Compute items liked by neighbors
-
- 
+# Compute items liked by neighbors
