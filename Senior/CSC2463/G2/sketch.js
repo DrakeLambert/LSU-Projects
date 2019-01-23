a3a1c5798a0d9b1ac917daedc3e45e3db38b5b27
Drake Lambert <drake.askme2@gmail.com>
2019-01-23 13:48:48 -0600

    added g2

---
+++ Senior/CSC2463/G2/sketch.js
@@ -0,0 +1,25 @@
+const possibleCursorColors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'magenta', 'brown', 'white', 'black']
+let cursorColor = possibleCursorColors[0];
+let colorOptionBoxes = possibleCursorColors.map()
+
+function setup() {
+	createCanvas(500, 500)
+	background('white');
+
+
+
+}
+
+function draw() {
+
+}
+
+class ColorOptionBox {
+	constructor(color) {
+		this.color = color;
+	}
+
+	draw(x, y) {
+
+	}
+}
