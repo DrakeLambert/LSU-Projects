1c2606370f9640cfc54c7643ff8ce2444db3fbb1
Drake Lambert <drake-lambert@outlook.com>
2019-01-24 14:26:50 -0600

    g2 done

--- Senior/CSC2463/G2/sketch.js
+++ Senior/CSC2463/G2/sketch.js
@@ -1,25 +1,54 @@
+class ColorOptionBox {
+	constructor(color, x, y, size) {
+		this.color = color;
+		this.x = x;
+		this.y = y;
+		this.size = size
+	}
+
+	draw() {
+		push();
+		stroke('whitesmoke');
+		strokeWeight(2);
+		fill(this.color);
+		rect(this.x, this.y, this.size, this.size);
+		pop();
+	}
+
+	containsPoint(x, y) {
+		return x <= this.x + this.size && x >= this.x && y <= this.y + this.size && y >= this.y;
+	}
+}
+
 const possibleCursorColors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'magenta', 'brown', 'white', 'black']
+const colorOptionBoxes = possibleCursorColors.map((color, i) => new ColorOptionBox(color, 2, 2 + 2 * i + i * 20, 20));
 let cursorColor = possibleCursorColors[0];
-let colorOptionBoxes = possibleCursorColors.map()
 
 function setup() {
-	createCanvas(500, 500)
-	background('white');
-
-
-
+	document.body.style.margin = 0;
+	createCanvas(windowWidth - 10, windowHeight - 10);
+	background('whitesmoke');
 }
 
 function draw() {
-
+	colorOptionBoxes.forEach((box, i) => {
+		box.draw();
+	});
 }
 
-class ColorOptionBox {
-	constructor(color) {
-		this.color = color;
+function mouseClicked() {
+	for (box of colorOptionBoxes) {
+		if (box.containsPoint(mouseX, mouseY)) {
+			cursorColor = box.color;
+			break;
+		}
 	}
+}
 
-	draw(x, y) {
-
-	}
+function mouseDragged() {
+	push();
+	stroke(cursorColor);
+	strokeWeight(10);
+	line(pmouseX, pmouseY, mouseX, mouseY);
+	pop();
 }
