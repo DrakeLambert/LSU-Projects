29f8449b96f7b3820dc2567914b7615b24582955
Drake Lambert <drake-lambert@outlook.com>
2019-02-19 14:35:34 -0600

    a2

--- Senior/CSC2463/A2/sketch.js
+++ Senior/CSC2463/A2/sketch.js
@@ -1,26 +1,72 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    let effects = [
-        new Tone.BitCrusher(4),
-        new Tone.PitchShift(.5),
-        new Tone.Vibrato(50, 10),
-        new Tone.Distortion(10)
+    const namedEffects = [
+        [new Tone.BitCrusher(4), 'Bit Crusher'],
+        [new Tone.PitchShift(.5), 'Pitch Shift'],
+        [new Tone.Vibrato(50, 10), 'Vibrato'],
+        [new Tone.Distortion(10), 'Distortion']
     ];
+
+    fill('black');
+    textAlign(LEFT, TOP);
+    textSize(20);
+
+    namedEffects.forEach((namedEffect, i) => {
+        let [effect, name] = namedEffect;
+        text(name, 10, i * 100 + 50);
+
+        let slider = createSlider(0, 100, 50);
+        slider.position(10, i * 100 + 80);
+        slider.style('width', '80px');
+        slider.elt.addEventListener('input', () => {
+            effect.wet.value = slider.value() / 100;
+        });
+    });
+
+    const effects = namedEffects.map((namedEffect) => {
+        return namedEffect[0];
+    });
+
+    effects.push(Tone.Master);
 
     effects.reduce((previous, next) => {
         previous.connect(next);
         return next;
     });
 
-    let notes = ['C', 'D', 'E', 'F', 'G'].map((note) => {
-        note += '4';
-        return new Tone.FatOscillator(note, 'sine', 'square');
+    const firstEffect = effects[0];
+
+    const keys = [
+        ['C', 'a'],
+        ['D', 's'],
+        ['E', 'd'],
+        ['F', 'f'],
+        ['G', 'g']
+    ].map((noteKey, i) => {
+        const note = noteKey[0] + '4';
+        const key = noteKey[1];
+        const envelope = new Tone.AmplitudeEnvelope().connect(firstEffect);
+        new Tone.Oscillator(note).connect(envelope).start();
+
+        const triggerButton = createButton(note);
+        triggerButton.position(10 + i * 50, 10);
+        triggerButton.mousePressed(() => { envelope.triggerAttack(); });
+        triggerButton.mouseReleased(() => { envelope.triggerRelease(); });
+        window.addEventListener('keydown', (event) => {
+            if (event.key === key) {
+                envelope.triggerAttack();
+            }
+        });
+        window.addEventListener('keyup', (event) => {
+            if (event.key === key) {
+                envelope.triggerRelease();
+            }
+        });
+        return key;
     });
 
-    fill('black');
-    textAlign(LEFT, TOP);
-    textSize(20);
+    text('Use keys: ' + keys.reduce((x, xs) => { return xs + ', ' + x; }), 300, 12.5);
 
     // notes.forEach((song, i) => {
 
