08bd463355569fafb141673337a2500954f26676
DrakeLambert <drake.askme2@gmail.com>
2019-03-01 11:15:26 -0600

    a3

--- Senior/CSC2463/A3/sketch.js
+++ Senior/CSC2463/A3/sketch.js
@@ -2,6 +2,10 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
+const probability = (probability) => {
+    return Math.random() <= probability;
+}
+
 const heartBeatSynth = (note) => {
     const options = { pitchDecay: 0.05, octaves: 10, oscillator: { type: 'sine' }, envelope: { attack: 0.001, decay: 0.4, sustain: 0.01, release: .1, attackCurve: 'exponential' } };
     const heartBeat = new Tone.MembraneSynth(options).toMaster()
@@ -10,18 +14,37 @@ const heartBeatSynth = (note) => {
     };
 };
 
-const heartBeatLow = heartBeatSynth(40);
-const heartBeatHigh = heartBeatSynth(50);
+const heartBeatLow = heartBeatSynth(10);
+const heartBeatHigh = heartBeatSynth(25);
 
+const gunShotSynth = () => {
+    const pitchShift = new Tone.PitchShift(0).toMaster();
 
+    const options = { noise: { type: 'white' }, envelope: { attack: 0.005, decay: 0.1, sustain: 0 } };
+    const gunShot = new Tone.NoiseSynth(options).connect(pitchShift);
+    const options2 = { noise: { type: 'brown' }, envelope: { attack: 0.05, decay: 0.5, sustain: 0, release: .01 } };
+    const gunShot2 = new Tone.NoiseSynth(options2).connect(pitchShift);
+    return () => {
+        if (probability(.3)) {
+            pitchShift.pitch = Math.random() * .1 - .1;
+            gunShot.triggerAttackRelease("2n");
+            gunShot2.triggerAttackRelease("2n");
+        }
+    };
+}
+
+const gunShot = gunShotSynth();
 
 Tone.Transport.scheduleRepeat(heartBeatLow, "2n");
 Tone.Transport.scheduleRepeat(heartBeatHigh, "2n", "8n");
+Tone.Transport.scheduleRepeat(gunShot, "16n");
 
-function mousePressed() {
+const image = [...document.getElementsByTagName('img')][0];
+function mousePressed () {
     if (Tone.Transport.state !== 'started') {
         Tone.Transport.start();
     } else {
         Tone.Transport.stop();
     }
+    image.hidden = !image.hidden;
 }
